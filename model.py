e1b69f9aa3237550dba2d1ede52e3ee85c76a8ed
ABDALLAH ABOUDRARI <aboudrariabdallah1@gmail.com>
2026-06-29 21:40:16 +0300

    Step 38: pick_block_size

--- model.py
+++ model.py
@@ -283,8 +283,11 @@ def slice_train_and_val(data, split_idx):
     # TODO: return (data[:split_idx], data[split_idx:])
     return (data[:split_idx],data[split_idx:])
 
-# Step 38 - pick_block_size (not yet solved)
-# TODO: implement
+# Step 38 - pick_block_size
+def pick_block_size(default_size):
+    if default_size < 1:
+        return 1
+    return default_size
 
 # Step 39 - slice_x_at_offset (not yet solved)
 # TODO: implement
