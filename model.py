d8de69f004a18f37637abfa1efa2bb17d53bbc70
ABDALLAH ABOUDRARI <aboudrariabdallah1@gmail.com>
2026-08-06 14:48:43 +0300

    Step 121: get_multihead_n_heads

--- model.py
+++ model.py
@@ -1108,8 +1108,10 @@ import numpy as np
 def transpose_heads_to_front(x):
     return np.ascontiguousarray(x.transpose(0, 2, 1, 3))
 
-# Step 121 - get_multihead_n_heads (not yet solved)
-# TODO: implement
+# Step 121 - get_multihead_n_heads
+def get_multihead_n_heads(config):
+    """Return the number of attention heads from a multi-head config."""
+    return config["n_heads"]
 
 # Step 122 - get_multihead_sequence_length (not yet solved)
 # TODO: implement
