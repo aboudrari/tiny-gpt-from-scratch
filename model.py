ec73d2e1496e1c9b722d83900fa385c65ce745ef
ABDALLAH ABOUDRARI <aboudrariabdallah1@gmail.com>
2026-08-07 14:22:04 +0300

    Step 126: transpose_heads_to_back

--- model.py
+++ model.py
@@ -1155,8 +1155,9 @@ def multihead_weighted_sum(weights, v_heads):
     """
     return np.matmul(weights, v_heads)
 
-# Step 126 - transpose_heads_to_back (not yet solved)
-# TODO: implement
+# Step 126 - transpose_heads_to_back
+def transpose_heads_to_back(x):
+    return np.transpose(x, (0, 2, 1, 3))
 
 # Step 127 - get_multihead_output_sequence_length (not yet solved)
 # TODO: implement
